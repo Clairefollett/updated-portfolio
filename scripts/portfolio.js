37464d0e55436865d9085a5d3b6d9d8891d598cb
Clarefollett <chlaw101@gmail.com>
2017-01-07 17:39:13 -0800

    working on ajax and I have no idea why it's not working

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,4 +1,4 @@
-var portfolio = [];
+//var portfolio = [];
 
 function Portfolio (opts) {
   this.title = opts.title;
@@ -9,25 +9,28 @@ function Portfolio (opts) {
   this.body = opts.body;
 }
 
-Portfolio.prototype.toHtml = function () {
-  var source = $('#article-template').html();
-  var template = Handlebars.compile(source);
+Portfolio.portfolios = [];
 
+Portfolio.prototype.toHtml = function (scriptTemplateId) {
+  //var source = $('#article-template').html();
+  //var template = Handlebars.compile(source);
+  var template = Handlebars.compile($(scriptTemplateId).text());
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  var html = template(this);
-
-  return html;
+  //var html = template(this);
+  this.body = marked(this.body);
+  return template(this);
+  //return html;
 };
+Portfolio.sortDataAndPush = function(localData) {
+  localData.sort(function(a, b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
 
-data.sort(function(a, b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
-
-data.forEach(function(ele) {
-  portfolio.push(new Portfolio(ele));
-});
-
-portfolio.forEach(function(a) {
-  $('#articles').append(a.toHtml());
-});
+  localData.forEach(function(ele) {
+    Portfolio.portfolios.push(new Portfolio(ele));
+  });
+};
+//portfolio.forEach(function(a) {
+  //$('#articles').append(a.toHtml());
+//});
--- scripts/portfolioView.js
+++ scripts/portfolioView.js
@@ -1,34 +1,33 @@
 var portfolioView = {};
 
-portfolioView.populateFilters = function() {
-  $('article').not('.template').each(function() {
-    var authorName, category, optionTag;
-    authorName = $(this).find('address a').text();
-    optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
-    $('#author-filter').append(optionTag);
-    category = $(this).attr('data-category');
-    optionTag = '<option value="' + category + '">' + category + '</option>';
-    if ($('#category-filter option[value="' + category + '"]').length === 0) {
-      $('#category-filter').append(optionTag);
-    }
-  });
-};
+// portfolioView.populateFilters = function() {
+//   $('article').not('.template').each(function() {
+//     var authorName, category, optionTag;
+//     authorName = $(this).find('address a').text();
+//     optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+//     $('#author-filter').append(optionTag);
+//     category = $(this).attr('data-category');
+//     optionTag = '<option value="' + category + '">' + category + '</option>';
+//     if ($('#category-filter option[value="' + category + '"]').length === 0) {
+//       $('#category-filter').append(optionTag);
+//     }
+//   });
+// };
 
 portfolioView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
-
       $('article').hide();
+      $('article[data-author="' + $(this).val() + '"]').fadeIn(2000);
 
-      $('article').not('.template').each(function(){
-        var author = $(this).find('address a').text();
-        var selection = $('#author-filter').val();
-        if( author === selection ){
-          $(this).fadeIn(3000);
-        };
-      });
-    }
-    else {
+      // $('article').not('.template').each(function(){
+      //   var author = $(this).find('address a').text();
+      //   var selection = $('#author-filter').val();
+      //   if( author === selection ){
+      //     $(this).fadeIn(3000);
+        //};
+      //});
+    } else {
       $('article').not('.template').fadeIn(3000);
     }
     $('#category-filter').val('');
@@ -40,16 +39,16 @@ portfolioView.handleCategoryFilter = function() {
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn(2000);
 
-      $('article').not('.template').each(function(){
-        var category = $(this).attr('data-category');
-        var selection = $('#category-filter').val();
-        if( category === selection ){
-          $(this).fadeIn(3000);
-        };
-      });
-    }
-    else {
+    //   $('article').not('.template').each(function(){
+    //     var category = $(this).attr('data-category');
+    //     var selection = $('#category-filter').val();
+    //     if( category === selection ){
+    //       $(this).fadeIn(3000);
+    //     };
+    //   });
+    } else {
       $('article').not('.template').fadeIn(3000);
     }
     $('#author-filter').val('');
@@ -59,32 +58,57 @@ portfolioView.handleCategoryFilter = function() {
 
 portfolioView.handleMainNav = function () {
   $('.nav-menu').on('click', '.tab', function() {
-    $('.tab').show();
-    $(this).hide();
-
-    var content = $(this).attr('data-content');
-
     $('.tab-content').hide();
-    $('#' + content).fadeIn(3000);
+    var content = $(this).attr('data-content');
+    $('#' + content).fadeIn(2000);
   });
-
   $('.nav-menu .tab:first').click();
 };
 
 portfolioView.setTeasers = function() {
+  // $('.article-body *:nth-of-type(n+2)').hide();
+  //
+  // $('.read-on').on('click', function(event) {
+  //   event.preventDefault();
+  //   if ($(this).text() === 'Read on') {
+  //     $(this).parent().find('*').fadeIn();
+  //     $(this).hide();
+  $('h2').prev('p').remove();
+  $('h2').next('p').remove();
   $('.article-body *:nth-of-type(n+2)').hide();
-
-  $('.read-on').on('click', function(event) {
-    event.preventDefault();
-    if ($(this).text() === 'Read on') {
+  $('article').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    if($(this).text() === 'Read on →') {
       $(this).parent().find('*').fadeIn();
-      $(this).hide();
-    };
+    //$(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      },200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
   });
 };
 
-portfolioView.populateFilters();
-portfolioView.handleAuthorFilter();
-portfolioView.handleCategoryFilter();
-portfolioView.handleMainNav();
-portfolioView.setTeasers();
+portfolioView.renderIndex = function() {
+  Portfolio.portfolios.forEach(function(a) {
+    $('#articles').append(a.toHtml('#article-template'));
+    if($('#category-filter option:contains("'+ a.category + '")').length === 0) {
+      $('#category-filter').append(a.toHtml('#category-filter-template'));
+    };
+    if($('#author-filter option:contains("'+ a.author + '")').length === 0) {
+      $('#author-filter').append(a.toHtml('#author-filter-template'));
+    };
+  });
+//portfolioView.populateFilters();
+  portfolioView.handleAuthorFilter();
+  portfolioView.handleCategoryFilter();
+  portfolioView.handleMainNav();
+  portfolioView.setTeasers();
+};
+
+$.getJSON('data/portfolio.json').done( function(data) {
+  Portfolio.sortDataAndPush(data);
+  portfolioView.renderIndex();
+});
